396a72c0322038010952262e93accba101a5df75
MandalorianFighter <yanush.polishchuk@gmail.com>
2022-07-24 14:32:31 +0300

    test eloquent orm insert data

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use Auth;
+use Illuminate\Support\Carbon;
 
 class CategoryController extends Controller
 {
@@ -20,5 +23,18 @@ class CategoryController extends Controller
             'category_name.required' => 'Please, Input Category Name.',
             'category_name.max' => 'Category Name Less Than 255Chars.',
         ]);
+
+        Category::insert([
+            'category_name' => $request->category_name,
+            'user_id' => Auth::user()->id,
+            'created_at' => Carbon::now()
+        ]);
+
+        // $category = new Category();
+        // $category->category_name = $request->category_name;
+        // $category->user_id = Auth::user()->id;
+        // $category->save();
+
+        return Redirect()->back()->with('success', 'Category Inserted Successfully!');
     }
 }
